f9edfdbe2617cceb37776b6d11be6cd9e85a8cfc
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-10-31 21:11:35 -0700

    Update production configs

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,7 @@
 #
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#workers
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-worker_timeout = 30 if ENV['RAILS_ENV'] == "production"
+worker_timeout = 30 # if ENV['RAILS_ENV'] == "production"
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
