cdea45e91fa3147f51788334ebbe682d96d8edfd
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-11-04 01:32:48 -0800

    Try different :return_to location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     def authenticate_user
       @user = User.find_by(id: session[:user_id])
       if !@user
-        session[:return_to] ||= request.referer
+        session[:return_to] ||= request.url
         redirect_to login_url, notice: "Please log in to access this page"
       end
     end
