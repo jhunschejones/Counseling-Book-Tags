7120c94551fade40b9c6d621f16e42ebd7843730
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-11-15 19:32:43 -0800

    Finish Book model tests

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -2,12 +2,14 @@ require 'test_helper'
 
 # `bundle exec ruby -Itest /Users/jjones/Documents/GitHub/Counseling-Book-Tags/test/models/book_test.rb`
 class BookTest < ActiveSupport::TestCase
-  test "title_keywords ignores special characters and duplicate words" do
-    keywords = Book.title_keywords("Harry Potter and the Sourcerer's Stone (Harry Potter Book 1)")
-    assert_equal keywords, ["HARRY", "POTTER", "SOURCERERS", "STONE", "BOOK", "1"]
+  describe "title_keywords" do
+    test "ignores special characters and duplicate words" do
+      keywords = Book.title_keywords("Harry Potter and the Sourcerer's Stone (Harry Potter Book 1)")
+      assert_equal keywords, ["HARRY", "POTTER", "SOURCERERS", "STONE", "BOOK", "1"]
+    end
   end
 
-  describe "by_query_params title search" do
+  describe "by_query_params :title" do
     test "works with extra words" do
       result = Book.by_query_params({title: "Adventures in the Land of Ruby"})
       assert_equal result[0].title, "Adventures in Ruby"
@@ -24,7 +26,7 @@ class BookTest < ActiveSupport::TestCase
     end
   end
 
-  describe "by_query_params author search" do
+  describe "by_query_params :author" do
     setup do
       # J.K. Rowling, Harry Potter and the Sorcerer's Stone
       Book.create_goodreads_book(3)
@@ -48,7 +50,7 @@ class BookTest < ActiveSupport::TestCase
     end
   end
 
-  describe "by_query_params isbn search" do
+  describe "by_query_params :isbn" do
     test "returns books matching one isbn" do
       result = Book.by_query_params({isbn: "1235"})
       assert_equal result[0].title, "Adventures in Ruby"
@@ -57,6 +59,109 @@ class BookTest < ActiveSupport::TestCase
     test "doesn't return books that have no matching isbns" do
       isbns = Book.by_query_params({isbn: "1235"}).map { |book| book.isbns }.flatten
       refute isbns.include?("222")
+    end
+  end
+
+  describe "by_query_params unrecognized param" do
+    test "raises" do
+      exception = assert_raises(RuntimeError) { Book.by_query_params({cover: "image.jpg"}) }
+      assert_equal exception.message, "Unrecognized search type"
+    end
+  end
+
+  describe "find_or_create" do
+    test "raises for unrecognized source" do
+      exception = assert_raises(RuntimeError) { Book.find_or_create("library", "1") }
+      assert_equal exception.message, "Unrecognized source"
+    end
+
+    describe "when book does not exist in db" do
+      test "creates new book from goodreads" do
+        Book.find_or_create("goodreads", 3)
+        assert_equal Book.last.source_id, "3"
+        assert_equal Book.last.source, "goodreads"
+      end
+
+      test "creates new book from openlibrary" do
+        Book.find_or_create("openlibrary", "OL82592W")
+        assert_equal Book.last.source_id, "OL82592W"
+        assert_equal Book.last.source, "openlibrary"
+      end
+    end
+
+    describe "when book exists in db" do
+      setup do
+        # Douglas Adams, The Hitchhiker's Guide to the Galaxy
+        Book.create_goodreads_book(16)
+        # J.K. Rowling, Harry Potter and the Philosopher's Stone
+        Book.create_openlibrary_book("OL82592W")
+      end
+
+      test "returns existing goodreads book from db" do
+        before_count = Book.count
+        book = Book.find_or_create("goodreads", 16)
+        assert_equal Book.count, before_count
+        assert_equal book.title, "The Hitchhiker's Guide to the Galaxy (Hitchhiker's Guide to the Galaxy, #1)"
+      end
+
+      test "returns existing openlibrary book from db" do
+        before_count = Book.count
+        book = Book.find_or_create("openlibrary", "OL82592W")
+        assert_equal Book.count, before_count
+        assert_equal book.title, "Harry Potter and the Philosopher's Stone"
+      end
+    end
+  end
+
+  describe "database_or_external" do
+    test "raises for unrecognized source" do
+      exception = assert_raises(RuntimeError) { Book.database_or_external("library", "1") }
+      assert_equal exception.message, "Unrecognized source"
+    end
+
+    describe "when book does not exist in db" do
+      test "returns goodreads book" do
+        book = Book.database_or_external("goodreads", 3)
+        assert_equal book[:title], "Harry Potter and the Sorcerer's Stone (Harry Potter, #1)"
+        assert_nil book[:id] # no :id when not in database
+      end
+
+      test "returns openlibrary book" do
+        book = Book.database_or_external("openlibrary", "OL82592W")
+        assert_equal book[:title], "Harry Potter and the Philosopher's Stone"
+        assert_nil book[:id] # no :id when not in database
+      end
+
+      test "raises for bad goodreads source_id" do
+        exception = assert_raises(RuntimeError) { Book.database_or_external("goodreads", 0) }
+        assert_equal exception.message, "Unable to find book with Goodreads id '0'"
+      end
+
+      test "raises for bad openlibrary source_id" do
+        exception = assert_raises(RuntimeError) { Book.database_or_external("openlibrary", 0) }
+        assert_equal exception.message, "Unable to find book with Openlibrary id '0'"
+      end
+    end
+
+    describe "when book exists in db" do
+      setup do
+        # Douglas Adams, The Hitchhiker's Guide to the Galaxy
+        Book.create_goodreads_book(3)
+        # J.K. Rowling, Harry Potter and the Philosopher's Stone
+        Book.create_openlibrary_book("OL82592W")
+      end
+
+      test "returns goodreads book" do
+        book = Book.database_or_external("goodreads", 3)
+        assert_equal book[:title], "Harry Potter and the Sorcerer's Stone (Harry Potter, #1)"
+        refute_nil book[:id] # :id only exists on valid DB record
+      end
+
+      test "returns openlibrary book" do
+        book = Book.database_or_external("openlibrary", "OL82592W")
+        assert_equal book[:title], "Harry Potter and the Philosopher's Stone"
+        refute_nil book[:id] # :id only exists on valid DB record
+      end
     end
   end
 end
