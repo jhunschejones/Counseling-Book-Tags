d020c27c7ea6723480d80d1346362a228843c6f2
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-11-20 16:02:14 -0800

    Add users controller tests and refactor
    * Refactor update logic to work on name and email updates seperately and increase readablility
    * Flash on user show or edit for incorrect user ID

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,31 +6,35 @@ class UsersController < ApplicationController
   end
 
   def show
-    redirect_to user_path(@user) if params[:id].to_i != @user.id
+    if params[:id].to_i != @user.id
+      flash[:alert] = "You cannot access information for another user"
+      redirect_to user_path(@user)
+    end
   end
 
   def edit
-    redirect_to edit_user_path(@user) if params[:id].to_i != @user.id
+    if params[:id].to_i != @user.id
+      flash[:alert] = "You cannot edit information for another user"
+      redirect_to edit_user_path(@user)
+    end
   end
 
   def update
-    if params[:user][:name] != @user.name
-      @user.update(name: params[:user][:name])
-    elsif params[:user][:email] != @user.email
-      # need to verify it's okay to update email in a way that is
-      # secure and won't leave users locked out of their accounts
-      users_with_email = User.where("email = :email AND id != :id OR unconfirmed_email = :email AND id != :id", email: params[:user][:email], id: @user.id)
+    if user_params[:name] && user_params[:name] != @user.name
+      @user.update(name: user_params[:name])
+    end
 
-      if users_with_email.empty?
-        @user.unconfirmed_email = params[:user][:email]
-        @user.generate_email_verification_token!
-        UserMailer.welcome_email(@user).deliver_later
-        flash[:notice] = "Please follow the verification link sent to your new email to confirm the change"
-      else
+    if user_params[:email] && user_params[:email] != @user.email
+      unless email_is_unique?
         flash[:alert] = "Emails must be unique, please request a password reset if you cannot access your account"
-        redirect_to edit_user_path(@user) and return
+        return redirect_to edit_user_path(@user)
       end
 
+      # securely verify new email without locking users out of their accounts
+      @user.unconfirmed_email = user_params[:email]
+      @user.generate_email_verification_token!
+      UserMailer.welcome_email(@user).deliver_later
+      flash[:notice] = "Please follow the verification link sent to your new email to confirm the change"
     end
 
     redirect_to user_path(@user)
@@ -49,6 +53,11 @@ class UsersController < ApplicationController
   end
 
   private
+    def email_is_unique?
+      User.where(
+        "email = :email AND id != :id OR unconfirmed_email = :email AND id != :id",
+        email: user_params[:email], id: @user.id).empty?
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,149 @@
 require 'test_helper'
 
+# bundle exec ruby -Itest /Users/jjones/Documents/GitHub/Counseling-Book-Tags/test/controllers/users_controller_test.rb
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get signup page" do
+  test "gets signup page" do
     get users_new_url
     assert_response :success
   end
 
-  # test "should get create" do
-  #   get users_create_url
-  #   assert_response :success
-  # end
+  describe "when creating a new user" do
+    test "redirects for login" do
+      post "/users/new", params: { user: { name: "new user", email: "newuser@dafox.com", password: "secret", password_confirmation: "secret" } }
+      follow_redirect!
+      assert_equal "/login", path
+    end
 
+    test "creates unverified user record" do
+      post "/users/new", params: { user: { name: "new user", email: "newuser@dafox.com", password: "secret", password_confirmation: "secret" } }
+      assert_equal "newuser@dafox.com", User.last.email
+      refute User.last.verified
+    end
+
+    describe "when there are user model errors" do
+      test "reloads new user page with error information" do
+        post "/users/new", params: { user: { name: "new user", email: "newuser@dafox.com", password: "secret", password_confirmation: "oops" } }
+        assert_select "div.new-user"
+        assert_select "div#error_explanation", /Password confirmation doesn't match Password/
+      end
+
+      test "doesn't create a new user" do
+        post "/users/new", params: { user: { name: "new user", email: "newuser@dafox.com", password: "secret", password_confirmation: "oops" } }
+        assert_empty User.where(email: "newuser@dafox.com")
+      end
+    end
+  end
+
+  test "gets user details page" do
+    login_as(users(:one))
+    get user_url(users(:one))
+    assert_response :success
+    assert_select "h2.user-details-title"
+    assert_select "p.name", /#{users(:one).name}/
+    assert_select "p.email", /#{users(:one).email}/
+    assert_select "p.created-at", /#{users(:one).created_at.localtime.to_formatted_s(:long_ordinal)}/
+  end
+
+  test "doesnt show user details for another user" do
+    login_as(users(:one))
+    get user_url(users(:two))
+    follow_redirect!
+    assert_equal "/users/#{users(:one).id}", path
+    assert_equal "You cannot access information for another user", flash[:alert]
+  end
+
+  test "gets user edit page" do
+    login_as(users(:one))
+    get edit_user_url(users(:one))
+    assert_response :success
+    assert_select 'input#user_name' do
+      assert_select "[value=?]", users(:one).name
+    end
+    assert_select 'input#user_email' do
+      assert_select "[value=?]", users(:one).email
+    end
+  end
+
+  test "doesnt allow edit page for a different user" do
+    login_as(users(:one))
+    get edit_user_url(users(:two))
+    follow_redirect!
+    assert_equal "/users/#{users(:one).id}/edit", path
+    assert_equal "You cannot edit information for another user", flash[:alert]
+  end
+
+  test "updates user name and redirect to user details page" do
+    new_user_name = "Comrade Carl"
+    login_as(users(:one))
+    patch "/users/#{users(:one).id}", params: { user: { name: new_user_name } }
+    follow_redirect!
+    assert_equal "/users/#{users(:one).id}", path
+    assert_select "h2.user-details-title"
+    assert_select "p.name", /#{new_user_name}/
+  end
+
+  describe "when updating email" do
+    describe "for non-unique new email" do
+      test "does not update email" do
+        origional_email = User.find(users(:one).id).email
+        login_as(users(:one))
+        patch "/users/#{users(:one).id}", params: { user: { email: users(:two).email } }
+        assert_equal origional_email, User.find(users(:one).id).email
+      end
+
+      test "redirects to user details page with instructions" do
+        origional_email = User.find(users(:one).id).email
+        login_as(users(:one))
+        patch "/users/#{users(:one).id}", params: { user: { email: users(:two).email } }
+        follow_redirect!
+
+        assert_equal "/users/#{users(:one).id}/edit", path
+        assert_equal "Emails must be unique, please request a password reset if you cannot access your account", flash[:alert]
+        assert_select 'input#user_email' do
+          assert_select "[value=?]", origional_email
+        end
+      end
+    end
+
+    describe "for uniqnue new email" do
+      test "enqueues verification email" do
+        login_as(users(:one))
+        assert_enqueued_emails 0
+        patch "/users/#{users(:one).id}", params: { user: { email: "carl+dev@dafox.com" } }
+        assert_enqueued_emails 1
+      end
+
+      test "saves unconfirmed email and verification token to user record" do
+        new_email = "carl+dev@dafox.com"
+        assert_nil User.find(users(:one).id).email_verification_token
+        login_as(users(:one))
+        patch "/users/#{users(:one).id}", params: { user: { email: new_email } }
+        assert_equal new_email, User.find(users(:one).id).unconfirmed_email
+        assert_not_nil User.find(users(:one).id).email_verification_token
+      end
+
+      test "does not change user email before verification" do
+        origional_email = User.find(users(:one).id).email
+        login_as(users(:one))
+        patch "/users/#{users(:one).id}", params: { user: { email: "carl+dev@dafox.com" } }
+        assert_equal origional_email, User.find(users(:one).id).email
+        assert_not_nil User.find(users(:one).id).email_verification_token
+      end
+
+      test "redirects to user details page with instructions" do
+        origional_email = User.find(users(:one).id).email
+        login_as(users(:one))
+        patch "/users/#{users(:one).id}", params: { user: { email: "carl+dev@dafox.com" } }
+        follow_redirect!
+        assert_equal "Please follow the verification link sent to your new email to confirm the change", flash[:notice]
+        assert_equal "/users/#{users(:one).id}", path
+        assert_select "p.email", /#{origional_email}/
+      end
+    end
+  end
+
+  private
+    def login_as(user)
+      post "/login", params: { email: user.email, password: "secret" }
+    end
 end
